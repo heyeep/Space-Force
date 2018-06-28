c110914c3a833c1663521aff335fec85ca995077
Hoang <baden009@yahoo.com>
2018-06-28 08:51:19 +0000

    Update SpaceForceGame.java

--- src/SpaceForceGame.java
+++ src/SpaceForceGame.java
@@ -2,13 +2,15 @@ import javax.swing.*;
 
 public class SpaceForceGame {
     public static void main(String args[]) {
-        Window window = new Window(Window.DEFAULT_WIDTH,
-                                   Window.DEFAULT_HEIGHT);
+       // Window window = new Window(Window.DEFAULT_WIDTH,
+        //                           Window.DEFAULT_HEIGHT);
         GameScene gScene = new GameScene(Window.DEFAULT_WIDTH,
                                          Window.DEFAULT_HEIGHT);
         gScene.revalidate();
         gScene.repaint();
-        window.addScene(gScene);
+        //window.addScene(gScene);
+        Window window = new Window(Window.DEFAULT_WIDTH,
+                                   Window.DEFAULT_HEIGHT, gScene);
     }
 
 }
