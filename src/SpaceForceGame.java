55e0bace78333380d0c3a2b6038f6f09d404b551
Hoang <baden009@yahoo.com>
2018-07-11 22:07:05 +0000

    Update SpaceForceGame.java to open the MainJFrame()

--- src/SpaceForceGame.java
+++ src/SpaceForceGame.java
@@ -4,13 +4,17 @@ public class SpaceForceGame {
     public static void main(String args[]) {
        // Window window = new Window(Window.DEFAULT_WIDTH,
         //                           Window.DEFAULT_HEIGHT);
-        GameScene gScene = new GameScene(Window.DEFAULT_WIDTH,
+        /*GameScene gScene = new GameScene(Window.DEFAULT_WIDTH,
                                          Window.DEFAULT_HEIGHT);
         gScene.revalidate();
         gScene.repaint();
         //window.addScene(gScene);
         Window window = new Window(Window.DEFAULT_WIDTH,
                                    Window.DEFAULT_HEIGHT, gScene);
+        */                           
+        MainFrame mf = new MainFrame();
+        mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mf.setVisible(true);
     }
 
 }
