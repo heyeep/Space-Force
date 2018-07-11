43995d803b725e46d498382c8dfa01b65a16675a
Mark Chang <hydryu@gmail.com>
2018-07-11 19:14:55 +0000

    Add new file

---
+++ src/model/Weak_Invader.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author Mark Chang
+ */
+public class Weak_Invader extends Invader {
+    int speed; //Speed at which the invader travels at, units: Pixels  
+   public Weak_Invader() {
+        super();
+        this.name = "WeakInvader";
+        hp = 20;
+        maxHp = 100;
+        speed = 5;
+    }
+
+    public Weak_Invader(String _name) {
+        super(_name);
+    }
+
+    public Weak_Invader(String _name, String _description) {
+        super(_name, _description);
+        this.init();
+    }
+
+    public void init() {
+        System.out.println("Player.init()");
+        this.setCoord(10, 10);
+    }
+
+    public void draw() {
+        
+    }
+
+    public void resize() {
+        
+    }
+
+    // TODO: Draw for Invader
+    // Have to fix the changing of icons
+    // inside Atom first
+}
+
