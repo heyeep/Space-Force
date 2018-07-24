bc6149fbac0c3c7f495b2867f99e85e91bd75f74
Hiep Nguyen <yzmism@gmail.com>
2018-07-24 16:11:15 -0700

    Add projectile file by Navid

---
+++ src/model/Projectile.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Nave
+ */
+public class Projectile extends Object{
+
+    private double x;
+    private double y;
+
+    public Projectile(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Projectile() {}
+
+    public void tick()
+    {
+        y -= 10;
+    }
+
+    public void init(){
+        System.out.print("Projectile.init()");
+        this.setCoord(274, 400);
+        this.setImage("bullet.png");
+        System.out.println("Projectile Width: " + getIcon().getIconWidth());
+        System.out.println("Projectile Height: " + getIcon().getIconHeight());
+    }
+
+    public void resize(){    }
+
+    void remove( ) {
+        //draw();
+        //revalidate();
+    }
+
+    @Override
+    public void draw() {
+    }
+}
