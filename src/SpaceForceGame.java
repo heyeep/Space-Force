938cf2af8cd5d29c7c9c131d1bd3e2ce2e892238
Hiep Nguyen <yzmism@gmail.com>
2018-07-30 15:17:38 -0700

    Remove commented code

--- src/SpaceForceGame.java
+++ src/SpaceForceGame.java
@@ -4,14 +4,10 @@ public class SpaceForceGame {
     public static void main(String args[]) {
         SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
-
-                    // Window window = new Window(Window.DEFAULT_WIDTH,
-                    //                           Window.DEFAULT_HEIGHT);
                     GameScene gScene = new GameScene(Window.DEFAULT_WIDTH,
                                                      Window.DEFAULT_HEIGHT);
                     gScene.revalidate();
                     gScene.repaint();
-                    //window.addScene(gScene);
                     Window window = new Window(Window.DEFAULT_WIDTH,
                                                Window.DEFAULT_HEIGHT, gScene);
                 }
