38b21a7e3ea536b99fd0a85b8d68000ab96dd56e
Hiep Nguyen <yzmism@gmail.com>
2018-07-31 14:19:55 -0700

    Add Hud system

---
+++ src/model/Hud.java
@@ -0,0 +1,81 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
+
+public class Hud extends Object implements MouseListener {
+    public Hud() {
+        this.init();
+    }
+
+    public Hud(String _name) {
+        super(_name);
+        this.init();
+    }
+
+    public Hud(String _name, ImageIcon _icon) {
+        super(_name, _icon);
+        this.init();
+    }
+
+    public Hud(String _name, String _description) {
+        super(_name, _description);
+        this.init();
+    }
+
+    public Hud(String _name, String _description, int _x, int _y) {
+        super(_name, _description, _x, _y);
+        this.init();
+    }
+
+    public Hud(String _name, String _description, String _fileName) {
+        super(_name, _description, _fileName);
+        this.init();
+    }
+
+    public Hud(String _name, String _description, String _fileName,
+               int _x, int _y) {
+        super(_name, _description, _fileName, _x, _y);
+        this.init();
+    }
+
+    public void init() {
+        this.setVisible(true);
+        addMouseListener(this);
+    }
+
+    public void draw(Graphics g) {
+    }
+
+    public void resize() {
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        this.doAction(this.getName());
+    }
+
+    public void doAction(String actionName) {
+        System.out.println("doAction: " + actionName);
+        switch (actionName) {
+        case "START":
+            // Switch to GameScene;
+            break;
+        default:
+            System.out.println("None");
+        }
+    }
+}
---
+++ src/systems/window/MainWindow.java
@@ -0,0 +1,91 @@
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+public class MainWindow extends Window {
+    JMenuBar menuBar;
+    JMenu startMenu;
+    JMenu startMenuLevelOfDifficulty;
+    JMenu scoresMenu;
+    JMenu helpMenu;
+    JMenuItem levelOfDifficultyGodMode;
+    JMenuItem levelOfDifficultyDeathMode;
+    JMenuItem levelOfDifficultyOne;
+    JMenuItem levelOfDifficultyTwo;
+    JMenuItem levelOfDifficultyThree;
+    JMenuItem levelOfDifficultyFour;
+    JMenuItem levelOfDifficultyFive;
+    JMenuItem scoresPersonalAchievements;
+    JMenuItem scoresBoard;
+    JMenuItem helpInstructions;
+    JMenuItem helpAbout;
+    public MainWindow() {
+        System.out.println("Main Window");
+        this.init();
+    }
+
+    public MainWindow(int _width, int _height) {
+        super(_width, _height);
+        System.out.println("Main Window");
+        this.init();
+    }
+
+    public MainWindow(int _width, int _height, Scene _scene) {
+        super(_width, _height, _scene);
+        System.out.println("Main Window");
+        this.init();
+    }
+
+    public void init() {
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setupMenus();
+    }
+
+    public void setupMenus() {
+        this.menuBar = new JMenuBar();
+        this.startMenu = new JMenu("Start Game");
+        this.startMenuLevelOfDifficulty = new JMenu("Leve of Difficulty");
+        this.scoresMenu = new JMenu("View Scores");
+        this.helpMenu = new JMenu("Help");
+        this.setupLevelOfDifficultyMenuItems();
+        this.setupScoresMenuItems();
+        this.setupHelpMenuItems();
+        this.menuBar.add(startMenu);
+        this.menuBar.add(scoresMenu);
+        this.menuBar.add(helpMenu);
+        this.setJMenuBar(this.menuBar);
+    }
+
+    public void setupLevelOfDifficultyMenuItems() {
+        this.levelOfDifficultyGodMode = new JMenuItem("God Mode");
+        this.levelOfDifficultyDeathMode = new JMenuItem("Death Mode");
+        this.levelOfDifficultyOne = new JMenuItem("Level 1");
+        this.levelOfDifficultyTwo = new JMenuItem("Level 2");
+        this.levelOfDifficultyThree = new JMenuItem("Level 3");
+        this.levelOfDifficultyFour = new JMenuItem("Level 4");
+        this.levelOfDifficultyFive = new JMenuItem("Level 5");
+        this.startMenu.add(this.levelOfDifficultyGodMode);
+        this.startMenu.add(this.levelOfDifficultyDeathMode);
+        this.startMenuLevelOfDifficulty.add(this.levelOfDifficultyOne);
+        this.startMenuLevelOfDifficulty.add(this.levelOfDifficultyTwo);
+        this.startMenuLevelOfDifficulty.add(this.levelOfDifficultyThree);
+        this.startMenuLevelOfDifficulty.add(this.levelOfDifficultyFour);
+        this.startMenuLevelOfDifficulty.add(this.levelOfDifficultyFive);
+        this.startMenu.add(this.startMenuLevelOfDifficulty);
+    }
+
+    public void setupScoresMenuItems() {
+        this.scoresPersonalAchievements = new JMenuItem("Personal Achievements");
+        this.scoresBoard = new JMenuItem("Score Board");
+        this.scoresMenu.add(this.scoresPersonalAchievements);
+        this.scoresMenu.add(this.scoresBoard);
+    }
+
+    public void setupHelpMenuItems() {
+        this.helpInstructions = new JMenuItem("Instructions");
+        this.helpAbout = new JMenuItem("About");
+        this.helpMenu.add(this.helpInstructions);
+        this.helpMenu.add(this.helpAbout);
+    }
+}
